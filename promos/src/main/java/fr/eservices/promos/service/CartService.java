e27b795941213669b70ddad2e93da1b4726d43a2
Theo Pires <theopires99@gmail.com>
2021-10-26 09:28:38 +0200

    controle des articles deja dans le panier

--- promos/src/main/java/fr/eservices/promos/service/CartService.java
+++ promos/src/main/java/fr/eservices/promos/service/CartService.java
@@ -1,6 +1,7 @@
 package fr.eservices.promos.service;
 
 import fr.eservices.promos.model.Cart;
+import fr.eservices.promos.model.CartElement;
 import fr.eservices.promos.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,16 @@ public class CartService {
 
     public void save(Cart cart) {
         cartRepository.save(cart);
+    }
+
+    public boolean isInCart(int customerId, int articeId) {
+        Cart cart = findByCustomerId(customerId);
+        if (cart==null) return false;
+        for (CartElement ce: cart.getElements()) {
+            if (ce.getArticle().getId()==articeId) {
+                return true;
+            }
+        }
+        return false;
     }
 }
